d258e31eebb4d4a2e79f6ee36f04bbacb5133ef2
Jeff Tratner <jtratner@counsyl.com>
2018-04-03 09:21:03 -0700

    Download full object rather than stream when hashing

--- pipenv/patched/piptools/repositories/pypi.py
+++ pipenv/patched/piptools/repositories/pypi.py
@@ -278,8 +278,8 @@ class PyPIRepository(BaseRepository):
 
     def _get_file_hash(self, location):
         h = hashlib.new(FAVORITE_HASH)
-        with open_local_or_remote_file(location, self.session) as fp:
-            for chunk in iter(lambda: fp.read(8096), b""):
+        with open_local_or_remote_file(location, self.session, 8096) as it:
+            for chunk in it:
                 h.update(chunk)
         return ":".join([FAVORITE_HASH, h.hexdigest()])
 
@@ -316,7 +316,7 @@ class PyPIRepository(BaseRepository):
 
 
 @contextmanager
-def open_local_or_remote_file(link, session):
+def open_local_or_remote_file(link, session, chunk_size):
     """
     Open local or remote file for reading.
 
@@ -334,12 +334,14 @@ def open_local_or_remote_file(link, session):
             raise ValueError("Cannot open directory for read: {}".format(url))
         else:
             with open(local_path, 'rb') as local_file:
-                yield local_file
+                for chunk in iter(lambda: local_file.read(8096), b""):
+                    yield chunk
     else:
         # Remote URL
         headers = {"Accept-Encoding": "identity"}
         response = session.get(url, headers=headers, stream=True)
         try:
-            yield response.raw
+            content = response.raw.read()
+            yield content
         finally:
             response.close()
